b89b3a8492d022c800a3de09edaaa7fe279b6cb2
mcm2020 <max.c.morrison@gmail.com>
2019-05-30 16:23:56 -0400

    updated focal length calculation

--- image-synthesis-Event-Signals/Assets/CaptureFramesAndPose/CaptureFramesAndPose.cs
+++ image-synthesis-Event-Signals/Assets/CaptureFramesAndPose/CaptureFramesAndPose.cs
@@ -6,15 +6,14 @@ using System.IO;
     For every frame:
     1. create an EXR file
     2. record pose
-    3. record current frame and associated EXR file
+    3. record the frame's time occurance (in seconds) and 
+       the frame's associated EXR file
 */
 [RequireComponent (typeof(Camera))]
 public class CaptureFramesAndPose : MonoBehaviour {
 
     public string folder = "FramesAndPose";
     public int frameRate = 1000;
-    public float xDimPixelsToDistance = 1;
-    public float yDimPixelsToDistance = 1;
     public enum OnSwitch {On, Off};
     public OnSwitch captureOn = OnSwitch.On;
 
@@ -25,6 +24,10 @@ public class CaptureFramesAndPose : MonoBehaviour {
     private StreamWriter matrixWriter;
     private StreamWriter poseWriter;
     private StreamWriter timeToFileWriter;
+    
+    private Rect rect;
+    private RenderTexture renderTexture;
+    private Texture2D screenshot;
 
     void Start()
     {
@@ -62,9 +65,12 @@ public class CaptureFramesAndPose : MonoBehaviour {
     }
 
     public void writeIntrinsicMatrix() {
+        // obtaining focal length and sensor size of the camera in millimeters
         float focalLength = cam.focalLength;
-        float xFocalLengthInPixels = xDimPixelsToDistance * focalLength;
-        float yFocalLengthInPixels = yDimPixelsToDistance * focalLength;
+        Vector2 sensorSize = cam.sensorSize;
+
+        // converting focal length in terms of mm to pixels 
+        float focalLengthInPixels = focalLength / sensorSize[0] * width; 
 
         Vector2 lensShift = cam.lensShift; // should be in center of screen
         float centerX = (width/2) + lensShift[0];
@@ -73,22 +79,22 @@ public class CaptureFramesAndPose : MonoBehaviour {
         // Assuming skew coefficient is 0
         int skewCoefficient = 0;
 
-        matrixWriter.WriteLine(xFocalLengthInPixels + " " + skewCoefficient + " " + centerX);
-        matrixWriter.WriteLine(0 + " " + yFocalLengthInPixels + " " + centerY);
+        matrixWriter.WriteLine(focalLengthInPixels + " " + skewCoefficient + " " + centerX);
+        matrixWriter.WriteLine(0 + " " + focalLengthInPixels + " " + centerY);
         matrixWriter.WriteLine(0 + " " + 0 + " " + 1);
         matrixWriter.WriteLine("");
 
         // Writing projection matrix for testing purposes
-        Matrix4x4 projectionMatrix = cam.projectionMatrix;
-        string[] rows = new string[4];
-        for (int i=0; i<4; i++) {
-            for (int j=0; j<4; j++) {
-                rows[i] += projectionMatrix[i, j].ToString("F") + " ";
-            }
-        }
-        for (int i=0; i<rows.Length; i++) {
-            matrixWriter.WriteLine(rows[i]);
-        }
+        // Matrix4x4 projectionMatrix = cam.projectionMatrix;
+        // string[] rows = new string[4];
+        // for (int i=0; i<4; i++) {
+        //     for (int j=0; j<4; j++) {
+        //         rows[i] += projectionMatrix[i, j].ToString("F") + " ";
+        //     }
+        // }
+        // for (int i=0; i<rows.Length; i++) {
+        //     matrixWriter.WriteLine(rows[i]);
+        // }
     }
 
     IEnumerator RecordFrame()
@@ -109,19 +115,30 @@ public class CaptureFramesAndPose : MonoBehaviour {
     }
 
     private void writeExrFile() {
-        var texture = ScreenCapture.CaptureScreenshotAsTexture();
-        
-        // Need to convert the texture from the screenshot into the format needed for EncodeToEXR()
-        Texture2D newTexture = new Texture2D(width, height, TextureFormat.RGBAFloat, false);
-        newTexture.SetPixels(texture.GetPixels());
-        newTexture.Apply();
+        if (renderTexture == null) {
+            // creates off-screen render texture that can rendered into
+            rect = new Rect(0, 0, width, height);
+            renderTexture = new RenderTexture(width, height, 24);
+            screenshot = new Texture2D(width, height, TextureFormat.RGBAFloat, false);
+        }
 
-        byte[] bytes = newTexture.EncodeToEXR(Texture2D.EXRFlags.CompressZIP);
+        cam.targetTexture = renderTexture;
+        cam.Render();
+        RenderTexture.active = renderTexture;
+        screenshot.ReadPixels(rect, 0, 0);
+
+        cam.targetTexture = null;
+        RenderTexture.active = null;
+
+        byte[] bytes = screenshot.EncodeToEXR(Texture2D.EXRFlags.CompressZIP);
         string fileName = string.Format("{0}/Frames/{1:D04}.exr", folderPath, Time.frameCount);
-        File.WriteAllBytes(fileName, bytes);
 
-        Object.DestroyImmediate(texture);
-        Object.DestroyImmediate(newTexture);
+        new System.Threading.Thread(() => {
+            // create file with image bytes
+            var f = System.IO.File.Create(fileName);
+            f.Write(bytes, 0, bytes.Length);
+            f.Close();
+        }).Start();
     }
 
     private void writeCameraPose() {
